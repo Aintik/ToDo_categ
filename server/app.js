cbf972f37b3329d6505c9ed2ead58815aff188d7
Aintik <abrorashrafhonov31@gmail.com>
2025-10-15 22:45:43 +0100

    cors updated

--- server/app.js
+++ server/app.js
@@ -26,7 +26,12 @@ const authRouter = require("./routes/auth")
 const app = express();
 
 app.use(logger("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://your-frontend-domain.com"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
